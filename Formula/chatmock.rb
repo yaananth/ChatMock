c2e769e49603e79f615e534c1e7f4ef2fe04ec71
Game_Time <108236317+RayBytes@users.noreply.github.com>
2025-09-16 20:58:10 +0500

    bump version

--- Formula/chatmock.rb
+++ Formula/chatmock.rb
@@ -3,8 +3,8 @@ class Chatmock < Formula
 
   desc "OpenAI & Ollama compatible API powered by your ChatGPT plan"
   homepage "https://github.com/RayBytes/ChatMock"
-  url "https://github.com/RayBytes/ChatMock/archive/refs/tags/v1.2.tar.gz"
-  sha256 "87e286337fc2fd8e1b543ecb382c5220995096636c36edf6367a4e7ce58ebe34"
+  url "https://github.com/RayBytes/ChatMock/archive/refs/tags/v1.3.tar.gz"
+  sha256 "9bb1a839f84e277ccd4233a7a0024759d9e2e60c9881ed1e20b57cd1b12033b4"
   license "MIT"
   head "https://github.com/RayBytes/ChatMock.git", branch: "main"
 
